7aebf43f600a3d92a37f3848af14bd805b2e8dd1
Dan Losev <shocker.nn@mail.ru>
2025-09-07 00:58:26 +0300

    Обновил структуру проекта

--- account/src/app.module.ts
+++ account/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {UserModule} from "./module/user/user.module";
+import {ConfigModule} from "@nestjs/config";
+import {DatabaseModule} from "./module/database/database.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), UserModule, DatabaseModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
